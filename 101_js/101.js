62adc4e90d604e84c5d53f6c1a5cdc587a9f3204
mohitfashal <110278030+mohitfashal@users.noreply.github.com>
2023-12-25 13:46:46 +0000

    hello

---
+++ 101_js/101.js
@@ -0,0 +1,11 @@
+const name = "Mohit"
+const repocount = 20
+// console.log(`my name is ${name} and my repo count is ${repocount}`);
+const game = new String('Mohitfashal')
+
+// console.log(game.concat(name));
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+const min = 10 
+const max = 20 
+console.log(Math.floor(Math.random() * max - min + min)) 
